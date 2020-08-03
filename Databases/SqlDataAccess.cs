6918406742ae5f096f561f54fcf9200715b4d27d
johnmorris4 <johnmorris4@bellsouth.net>
2020-08-03 05:50:58 -0400

    * SqlDataAccess.cs:
    * HotelAppLibrary.csproj: Setup of the Hotel App Library

--- Databases/SqlDataAccess.cs
+++ Databases/SqlDataAccess.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using Dapper;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 namespace HotelAppLibrary.Databases
 {
     public class SqlDataAccess
     {
-        public SqlDataAccess()
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
         {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
         }
     }
 }
