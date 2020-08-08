7aa63145a2b360c5904bf20fe7e6fd80a2c2a827
johnmorris4 <johnmorris4@bellsouth.net>
2020-08-08 05:11:21 -0400

    * HotelApp.sln: * SqlData.cs: * IDatabaseData.cs: * workspace.xml: * contentModel.xml: * 8e02f1ac-e018-4b41-a591-6398daf13a12.xml:
    * riderModule.iml: Added New modules to the project

---
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelAppWeb.Pages
+{
+    public class BookSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using HotelAppLibrary.Databases;
-using HotelAppLibrary.Models;
+using System.Text;
 
 namespace HotelAppLibrary.Data
 {
@@ -16,68 +17,69 @@ namespace HotelAppLibrary.Data
             _db = db;
         }
 
-
         public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
         {
-            return _db.LoadData<RoomTypeModel, dynamic>(
-                "dbo.spRoomTypes_GetAvailableTypes",
-                new { startDate, endDate },
-                                connectionStringName,
-                true);
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
+                                                 new { startDate, endDate },
+                                                 connectionStringName,
+                                                 true);
         }
 
-        public void BookGuest(
-            string firstName,
-            string lastName,
-            DateTime startDate,
-            DateTime endDate,
-            int roomTypeId
-            )
+        public void BookGuest(string firstName,
+                              string lastName,
+                              DateTime startDate,
+                              DateTime endDate,
+                              int roomTypeId)
         {
-            GuestModel guest = _db.LoadData<GuestModel, dynamic>(
-                "dbo.spGuest_Insert",
-                new { firstName, lastName },
-                connectionStringName,
-                true).First();
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("dbo.spGuests_Insert",
+                                                                 new { firstName, lastName },
+                                                                 connectionStringName,
+                                                                 true).First();
 
-            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>(
-                    "select * from dbo.RoomTypes were Id = @Id",
-                    new { Id = roomTypeId },
-                    connectionStringName,
-                    false
-            ).First();
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypes where Id = @Id",
+                                                                          new { Id = roomTypeId },
+                                                                          connectionStringName,
+                                                                          false).First();
 
             TimeSpan timeStaying = endDate.Date.Subtract(startDate.Date);
 
-            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>(
-                "dbo.spRooms_GetAvailableRooms",
-                   new { startDate, endDate, roomTypeId },
-                              connectionStringName,
-                true);
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                              new { startDate, endDate, roomTypeId },
+                                                                              connectionStringName,
+                                                                              true);
 
             _db.SaveData("dbo.spBookings_Insert",
-                new
-                {
-                    roomId = availableRooms.First().Id,
-                    guestId = guest.Id,
-                    startDate = startDate,
-                    endDate = endDate,
-                    totalCost = timeStaying.Days * roomType.Price
-                },
-                connectionStringName,
-                true);
+                         new
+                         {
+                             roomId = availableRooms.First().Id,
+                             guestId = guest.Id,
+                             startDate = startDate,
+                             endDate = endDate,
+                             totalCost = timeStaying.Days * roomType.Price
+                         },
+                         connectionStringName,
+                         true);
         }
 
         public List<BookingFullModel> SearchBookings(string lastName)
         {
             return _db.LoadData<BookingFullModel, dynamic>("dbo.spBookings_Search",
-                new { lastName, startDate = DateTime.Now.Date }, connectionStringName, true);
+                                                    new { lastName, startDate = DateTime.Now.Date },
+                                                    connectionStringName,
+                                                    true);
         }
 
         public void CheckInGuest(int bookingId)
         {
-            _db.SaveData("dbo.spBookings_CheckedIn",
-                new { Id = bookingId }, connectionStringName, true);
+            _db.SaveData("dbo.spBookings_CheckIn", new { Id = bookingId }, connectionStringName, true);
+        }
+
+        public RoomTypeModel GetRoomTypeById(int id)
+        {
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetById",
+                                                        new { id },
+                                                        connectionStringName,
+                                                        true).FirstOrDefault();
         }
     }
 }
