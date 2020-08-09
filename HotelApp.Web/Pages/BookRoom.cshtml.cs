c0e27461cdab128ea5db7e535a176726b70f5257
johnmorris4 <johnmorris4@bellsouth.net>
2020-08-09 03:43:34 -0400

    Setup new project for the Client

--- HotelApp.Web/Pages/BookRoom.cshtml.cs
+++ HotelApp.Web/Pages/BookRoom.cshtml.cs
@@ -1,12 +1,49 @@
-$HEADER$using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace $NAMESPACE$
+namespace HotelAppWeb.Pages
 {
-    public class $CLASS$ : PageModel
+    
+    
+    public class BookRoomModel : PageModel
     {
+        private readonly IDatabaseData _db;
+
+        [BindProperty(SupportsGet = true)]
+        public int RoomTypeId { get; set; }
+        [BindProperty]
+        public DateTime StartDate { get; set; } 
+        [BindProperty]
+        public DateTime EndDate { get; set; } 
+        [BindProperty]
+        public string FirstName { get; set; }
+        [BindProperty]
+        public string LastName { get; set; }
+        public RoomTypeModel RoomType { get; set; }
+
+
+
+        public BookRoomModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
+
         public void OnGet()
         {
-	          $END$
+            if (RoomTypeId > 0)
+            {
+                RoomType = _db.GetRoomTypeById(RoomTypeId);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.BookGuest(FirstName, LastName, StartDate, EndDate, RoomTypeId);
+            return RedirectToPage("/Index");
         }
     }
 }
--- HotelApp.Web/Pages/RoomSearch.cshtml.cs
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -1,12 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HotelAppWeb.Pages
 {
-    public class BookSearchModel : PageModel
+    public class RoomSearchModel : PageModel
     {
+        private readonly IDatabaseData _data;
+
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+
+        public List<RoomTypeModel> AvailableRoomTypes { get; set; }
+        
+        [BindProperty(SupportsGet = true)] 
+        public bool SearchEnabled { get; set; } = false;
+
+        public RoomSearchModel(IDatabaseData data)
+        {
+            _data = data;
+        }
         public void OnGet()
         {
+            if (SearchEnabled == true)
+            {
+               AvailableRoomTypes = _data.GetAvailableRoomTypes(StartDate, EndDate);
+                
+            }   
+        }
+
+        public IActionResult OnPost()
+        {
             
+            return RedirectToPage(new
+            {
+                SearchEnabled = true, 
+                StartDate = StartDate.ToString("yyyy-MM-dd"), 
+                EndDate = EndDate.ToString("yyyy-MM-dd")
+            });
         }
     }
 }
